91d3ebdb44e2c3fa2e4cf1bdf377c7db2b750d34
y-emam <YasserEmam2002@gmail.com>
2024-07-14 15:35:02 +0300

    fixed a bug

--- index2.js
+++ index2.js
@@ -284,8 +284,7 @@ const reloadEnv = () => {
   dotenv.config({ path: ".env" });
 };
 const getTransactions = async (wallet, time_limit) => {
-  console.log("Here");
-  // let base_url = `https://api.helius.xyz/v0/addresses/${wallet}/transactions?api-key=${process.env.HELIUS_KEY}`;
+  let base_url = `https://api.helius.xyz/v0/addresses/${wallet}/transactions?api-key=${process.env.HELIUS_KEY}`;
   let url = base_url;
   let lastSignature = null;
 
